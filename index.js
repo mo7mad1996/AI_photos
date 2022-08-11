6d06004dd3caf277e35946fa476a46d9892f0808
mo7mad1996 <mo7mad369@gmail.com>
2022-08-11 23:42:37 +0200

    actvate account

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
-const api_key = '53214894-1872-4441-a15d-f599eaf3862d'
+const api_key = 'b8ef7571-4b05-44be-82fb-a1f189faa691'
+// const api_key = '53214894-1872-4441-a15d-f599eaf3862d'
 
 const bodyParser = require('body-parser');
 const deepai = require('deepai'); // OR include deepai.min.js as a script tag in your HTML
 
 deepai.setApiKey(api_key);
+
+
+const fs = require('fs');
+
+
+
 
 const express = require('express')
 const app = express()
@@ -17,13 +24,14 @@ app.use(bodyParser.urlencoded({
 // parse application/json
 app.use(bodyParser.json());
 
-app.post('/api', (req, res) => {
-  deepai.callStandardApi("text2img", {
-    text: req.body.text,
-  }).then(data => res.json({
-    data
-  })).catch(err => res.status(401).json({
-    err
-  }))
+app.post('/api', async (req, res) => {
+
+  deepai
+    .callStandardApi("text2img", {
+      text: "car",
+    })
+    .then(data => console.log(data))
+    .catch(err => console.log(err))
+
 })
 app.listen(process.env.PORT || 3000)
