10e0eda0cb618db438c5c007e93eee9b12a65907
mo7mad1996 <mo7mad369@gmail.com>
2022-09-01 20:46:43 +0200

    remove API

--- index.js
+++ index.js
@@ -32,31 +32,3 @@ app.post('/api', async (req, res) => {
 })
 
 app.listen(process.env.PORT || 3000)
-
-const axios = require('axios')
-
-app.get('/test', (req, res) => {
-
-
-  axios.post(
-      'https://api.replicate.com/v1/predictions', // host URL
-      {
-        // data
-        "version": "5c7d5dc6dd8bf75c1acaa8565735e7986bc5b66206b55cca93cb72c9bf15ccaa",
-        "input": {
-          "text": "Cat"
-        }
-      }, {
-        // request headers
-        Headers: {
-          'Content-Type': 'application/json',
-          "Authorization": 'Token dfec5aa4019b735f9c6a0b2c9bfbdee97bb0cbda'
-        }
-      })
-    .then(res => res.json({
-      res
-    }))
-    .catch(err => res.json({
-      err
-    }))
-})
